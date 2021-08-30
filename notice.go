9f43879c9ec1c6fd87931215e3e6ba3390142cbe
Sakaizd <23312700+Sakaizd@users.noreply.github.com>
2021-08-30 15:56:18 +0800

    update notice

--- notice.go
+++ notice.go
@@ -9,8 +9,15 @@ import (
 
 func PostNotice(msg string, sckey string) {
 
+	var title string
+	if msg == "重复提交问卷" {
+		title = "健康状态申报重复"
+	} else if msg == "插入问卷数据成功" {
+		title = "您已完成今天的健康状态申报"
+	}
+
 	data := url.Values{}
-	data.Add("title", "check-in_results")
+	data.Add("title", title)
 	data.Add("desp", msg)
 
 	resp, err := http.PostForm("https://sctapi.ftqq.com/"+sckey+".send", data)
